b0ee6d466b8d07618b028334f6f263c1ab56e3d5
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-12-18 18:58:28 +0700

    fix: simplify array schema

--- src/array.ts
+++ src/array.ts
@@ -2,13 +2,9 @@ import type { SchemaObject } from ".";
 
 export type ArraySchema = {
   type: "array",
+  prefixItems?: SchemaObject[],
+  items?: SchemaObject | boolean,
   minItems?: number,
   maxItems?: number,
   uniqueItems?: boolean,
-} & ({
-  items?: SchemaObject,
-  prefixItems?: never,
-} | {
-  prefixItems: SchemaObject[],
-  items?: SchemaObject | false,
-});
+};
